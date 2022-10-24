77866105b6ed93b8aba86c907c8847c4c0cca965
Suleiman13 <suleimanidri3@gmail.com>
2022-10-24 07:20:45 -0700

    added DB

--- Config/db.js
+++ Config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect("mongodb+srv://suleiman:code2022@cluster0.pgmmcce.mongodb.net/?retryWrites=true&w=majority", {
+		const conn = await mongoose.connect("mongodb+srv://suleiman:code2022@cluster0.pgmmcce.mongodb.net/suleimanDB?retryWrites=true&w=majority", {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
 			// useCreateIndex: true,
