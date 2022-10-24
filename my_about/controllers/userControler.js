39a52dff1efaf0fdedb1e118505ea007c1701cad
Suleiman13 <suleimanidri3@gmail.com>
2022-10-24 06:22:05 -0700

    removed unwanted folder

--- my_about/controllers/userControler.js
+++
@@ -1,7 +0,0 @@
-import asyncHandler from 'express-async-handler';
-import bcrypt from 'bcryptjs';
-import {generateToken} from '../utilities/generate_token';
-import User from '../models/user';
-
-
-export const user_signup = asyncHandler(async(req, res) =)
