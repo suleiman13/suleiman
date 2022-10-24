bb68f2fd76b0dd8d2d65aac2cb073eb8bc6d5284
Suleiman13 <suleimanidri3@gmail.com>
2022-10-24 06:36:08 -0700

    changed PORT

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/user', user_router)
 app.use("/api/user", work_router)
 app.use("/api/item", item_router)
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen,(
     PORT,
